e64cb944780ea3dbe48ce31dbad39366a9c76a10
Tushar <tv4648551@gmail.com>
2024-12-22 17:37:26 +0530

    for deplyoment purposes

---
+++ backend/src/one.js
@@ -0,0 +1,44 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+
+import {connectDB} from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+})
+);
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port:"+PORT);
+    connectDB()
+});
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
+
+
+
